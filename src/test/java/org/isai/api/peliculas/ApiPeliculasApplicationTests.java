0e706b8f10a6414cf3428e31e83e625d881a8c31
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-04 10:15:24 -0500

    configuracion inicial

---
+++ src/test/java/org/isai/api/peliculas/ApiPeliculasApplicationTests.java
@@ -0,0 +1,13 @@
+package org.isai.api.peliculas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPeliculasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
